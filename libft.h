842a049af13ae2353d429a97d94fcdffcb9d28ac
Alexandru Ghergut <aghergut@c2r17s4.42madrid.com>
2024-10-17 16:42:56 +0200

    all libft

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aghergut <aghergut@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 18:46:30 by aghergut          #+#    #+#             */
+/*   Updated: 2024/10/16 16:09:33 by aghergut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include "changechar/changechar.h"
+# include "checkchar/checkchar.h"
+# include "lists/lists.h"
+# include "memory/memory.h"
+# include "strtoint/strtoint.h"
+# include "strings/strings.h"
+# include "puts/puts.h"
+# include "get_next_line/get_next_line.h"
+
+
+
+
+#endif
